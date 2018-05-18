5165529cc03a055a7e939fa00b5c65c7e898db33
Nathan Wentworth <nwentworth95@gmail.com>
2018-05-18 15:47:30 -0400

    added a lightbox for images in posts/projects

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -1,3 +1,14 @@
+'use strict'
+
+window.addEventListener('load', function () {
+  document.body.classList.add('js');
+  document.body.classList.remove('no-js');
+
+  let images = document.querySelectorAll('.content img');
+  for (var i = 0; i < images.length; i++) {
+    images[i].addEventListener('click', lightbox.setImg, false);
+  }
+});
 
 var lightbox = function () {
   var elem = document.getElementById('lightbox');
@@ -16,14 +27,18 @@ var lightbox = function () {
   init();
 
   function setImg(_img) {
-    img.src = _img;
+    if (_img.currentTarget) {
+      img.src = _img.currentTarget.src;
+    } else {
+      img.src = _img;
+    }
     lightbox.display(true);
   }
 
   function display(disp) {
     hidden = !disp;
     elem.classList.toggle('hidden', !disp)
-    // PreventScroll(disp);
+    document.body.classList.toggle('no-scroll', disp);
   }
 
   return {
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,54 +1,15 @@
-'use strict';
-
-var projectItems = document.getElementsByClassName('project-item');
-var filterElement = document.getElementById('filter');
-var currentFilter = '';
+'use strict'
 
 window.addEventListener('load', function () {
-  filterElement.addEventListener("click", function() {
-    clearFilters();
-  });
+  document.body.classList.add('js');
+  document.body.classList.remove('no-js');
 
-  var tagElements = document.getElementsByClassName('tag');
-  for (var i = 0; i < tagElements.length; i++) {
-    tagElements[i].addEventListener("click", function() {
-      redisplayProjects(this.dataset.tag);
-    });
+  let images = document.querySelectorAll('.content img');
+  for (var i = 0; i < images.length; i++) {
+    images[i].addEventListener('click', zoomImg, false);
   }
-}, false);
+});
 
-function redisplayProjects(tag) {
-  if (currentFilter != tag) {
-    currentFilter = tag;
+zoomImg(e) {
 
-    for (var i = 0; i < projectItems.length; i++) {
-      var includesTag = (!projectItems[i].dataset.tags.toLowerCase().includes(tag));
-      projectItems[i].classList.toggle('hidden', includesTag);
-
-      var projectChildren = projectItems[i].getElementsByTagName('span');
-      for (var j = 0; j < projectChildren.length; j++) {
-        var isTag = (projectChildren[j].dataset.tag === tag);
-        projectChildren[j].classList.toggle('selected', isTag);
-      }
-    }
-
-    filterElement.textContent = "filtering for: " + tag + " (click to clear)";
-    filterElement.classList.add('filter-on');
-  } else {
-    clearFilters();
-  }
-}
-
-function clearFilters() {
-  for (var i = 0; i < projectItems.length; i++) {
-    projectItems[i].classList.remove('hidden');
-
-    var projectChildren = projectItems[i].getElementsByTagName('span');
-    for (var j = 0; j < projectChildren.length; j++) {
-      projectChildren[j].classList.remove('selected');
-    }
-  }
-
-  currentFilter = "";
-  filterElement.classList.remove('filter-on');
 }
