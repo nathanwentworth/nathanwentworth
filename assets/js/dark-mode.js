ea88bd2f7054dedaa086b2d3c83994bd86835071
Nathan Wentworth <nwentworth95@gmail.com>
2018-10-26 15:04:59 -0400

    added dark mode

---
+++ assets/js/dark-mode.js
@@ -0,0 +1,11 @@
+function darkModeCheck() {
+  let time = new Date();
+  let hour = time.getHours();
+  if (hour < 6 || hour > 18) {
+    document.querySelector('html').classList.add('dark');
+  } else {
+    document.querySelector('html').classList.remove('dark');
+  }
+};
+darkModeCheck()
+// window.addEventListener('load', darkModeCheck, false);
