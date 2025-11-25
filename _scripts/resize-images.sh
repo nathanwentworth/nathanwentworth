6aba25b063dc04939a1df52e57173de7b57a2594
nathan wentworth <nwentworth95@gmail.com>
2025-11-25 07:35:24 -0500

    weeknotes 5

--- _scripts/resize-images.sh
+++ _scripts/resize-images.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+mkdir -p ../small
 mogrify -resize 1200x1200\> -path ../small *.jpeg
+mogrify -resize 1200x1200\> -path ../small *.jpg
+
+mkdir -p ../large
+mogrify -resize 1920x1920\> -path ../large *.jpeg
+mogrify -resize 1920x1920\> -path ../large *.jpg
